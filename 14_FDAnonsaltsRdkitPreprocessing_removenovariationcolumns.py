7e3b1585679b040c846f0ffc7b69a2d3193623d5
Antonia Gucic <antonia.gucic@hotmail.com>
2026-08-18 23:06:44 +0100

    Add files via upload
    Python script to remove molecular descriptors with zero variation

---
+++ 14_FDAnonsaltsRdkitPreprocessing_removenovariationcolumns.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+input_file = "13_FDAnonsalts_moldescr_Rdkit.csv"  # input file name
+output_file = "14_FDAnonsaltsRdkit_variation.csv"  # output file name
+
+# Read the CSV file
+df = pd.read_csv(input_file)
+
+# Remove columns where all values are the same
+df_cleaned = df.loc[:, (df.nunique() > 1)]
+
+# Save the cleaned DataFrame to a new CSV file
+df_cleaned.to_csv(output_file, index=False)
+
+print(f"Columns with unique values saved to {output_file}")
